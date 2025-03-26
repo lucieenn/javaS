c602df988a191f3d9d78e1499271d6ff82bf940c
Davi dos Santos Oliveira Silva <davi83242@gmail.com>
2025-03-26 11:53:41 -0300

    combustivel
    Fiz a conta para qual é mais vantajoso, caso seja a gasolina.
    ainda falta o alcool

--- combustiveis/combustiveis.js
+++ combustiveis/combustiveis.js
@@ -1,57 +1,17 @@
-let gasolina= window.document.getElementById('gasolinaP')
-let etanol = window.document.getElementById('etanolP')
-let porcentagem = window.document.getElementById('porcentagem')
-let resposta= window.document.getElementById('resposta')
+function calcular() {
+    const preco = parseFloat(document.getElementById('preco').value);
+    const combustivel = document.querySelector('input[name="combustivel"]:checked').value;
+    const porcentagem = parseFloat(document.getElementById('porcentagem').value) /100;
 
-function calcular(){
-  
-   switch (porcentagem){
-      case 0:
-          
-          break
-      case 1:
-          porcentagem=0.65
-          break
-      case 2:
-         porcentagem=0.66
-          break
-      case 3:
-          porcentagem=0.67
-          break
-      case 4:
-         porcentagem=0.68
-          break
-      case 5:
-         porcentagem=0.69
-          break
-      case 6:
-         porcentagem=0.70
-          break
-      case 7:
-         porcentagem=0.71
-          break
-          case 7:
-         porcentagem=0.72
-          break
-          case 7:
-         porcentagem=0.73
-          break
-          case 7:
-         porcentagem=0.74
-          break
-          case 7:
-         porcentagem=0.75
-          break
-  }
-  const diferenca= ((gasolina *100)/etanolp)*100
-   
-   if(diferenca<=30 && porcentagem>=0,65 && porcentagem<=0,70)
-   {
-    resposta.innerHTML='Abasteça com gasolina'
-   }
-   else{ 
-    
-    resposta.innerHTML='abasteça com alcool'
-   }
+    let resp;   
+
+    if (combustivel === 'gasolina') {
+        const alcoolpreco = preco * porcentagem 
+        resp = `É vantajoso abastecer com álcool se o preço for até R$ ${alcoolpreco.toFixed(2)}`;
+    } else {
+        const gasolinapreco =preco * porcentagem / 100;
+        resp = `É vantajoso abastecer com gasolina se o preço for até R$ ${gasolinapreco.toFixed(2)}`;
+    }
+
+    document.getElementById('resp').innerText = resp;
 }
-//se o preço da gasolina for 30% a mais que o acool, compensa para o usuario usar o alcool.
